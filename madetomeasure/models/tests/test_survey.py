fb0d5901257cc8ead9047ca523539df0525afcb4
Fredrik Nilson <fredrik@betahaus.net>
2011-09-23 10:47:18 +0200

    removint test for invite emails and invite message on survey

--- madetomeasure/models/tests/test_survey.py
+++ madetomeasure/models/tests/test_survey.py
@@ -34,24 +34,12 @@ class SurveyTests(unittest.TestCase):
         obj = self._make_obj()
         self.assertTrue(verifyObject(ISurvey, obj))
 
-    def test_invitation_emails(self):
-        obj = self._make_obj()
-        emails_field = "robin@betahaus.net\nfredrik@betahaus.net"
-        obj.set_invitation_emails(emails_field)
-        self.assertEqual(obj.get_invitation_emails(), emails_field)
-    
     def test_from_address(self):
         obj = self._make_obj()
         email_field = "noreply@betahaus.net"
         obj.set_from_address(email_field)
         self.assertEqual(obj.get_from_address(), email_field)
     
-    def test_mail_message(self):
-        obj = self._make_obj()
-        value = "Important message"
-        obj.set_mail_message(value)
-        self.assertEqual(obj.get_mail_message(), value)
-
     def test_finished_text(self):
         obj = self._make_obj()
         value = "We so happy now, okay?"
