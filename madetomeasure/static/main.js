aca882128c49c1e33b796db040160d4417c937d0
Robin Harms Oredsson <robin@betahaus.net>
2011-11-10 09:32:50 +0100

    JS with translations fixed

--- madetomeasure/static/main.js
+++ madetomeasure/static/main.js
@@ -1,7 +1,22 @@
+/* JS that should be present on every page, regardless of its function.*/
+var m2m = {};
+m2m.translation = {};
+
+/* Translations loader. This must be loaded before all other m2m js! */
+$(document).ready(function () {
+    $('.js_translation').each(function () {
+        $(this).children().each(function() {
+            var item = $(this);
+            var tkey = item.attr('class').replace('js_trans_', '');
+            m2m.translation[tkey] = item.text();
+        });
+    });
+});
+
 /* warn when user click previous */
 $(document).ready(function() {
     $("#deformprevious").live('click', function(event) {
-        if(!confirm(m2m.trans['confirm_back']))
+        if(!confirm(m2m.translation['confirm_back']))
             event.preventDefault();
     });
 });
