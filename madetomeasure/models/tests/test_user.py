cf76e38b3660991662203378cb318f8788164b68
Fredrik Nilson <sassur@gmail.com>
2011-11-30 15:05:59 +0100

    test for user model

--- madetomeasure/models/tests/test_user.py
+++ madetomeasure/models/tests/test_user.py
@@ -1,4 +1,6 @@
 import unittest
+from datetime import timedelta
+from datetime import datetime
 
 from pyramid import testing
 from zope.interface.verify import verifyObject
@@ -26,7 +28,71 @@ class UserTests(unittest.TestCase):
 
     def test_verify_obj(self):
         self.failUnless(verifyObject(IUser, self._cut()))
+        
+    def test_get_title(self):
+        user = self._cut()
+        fname = 'Kalle'
+        lname = 'Karlsson'
+        user.set_field_value('first_name', fname)
+        user.set_field_value('last_name', lname)
+        self.assertEqual(user.get_title(), "%s %s" % (fname, lname))
+        
+    def test_check_password(self):
+        self.config.scan('betahaus.pyracont.fields.password')
+        user = self._cut()
+        password = 'password1'
+        user.set_field_value('password', password)
+        self.assertTrue(user.check_password(password))
+        
+    def test_new_request_password_token(self):
+        user = self._cut()
+        user.set_field_value('email', 'test@test.com')
+        request = testing.DummyRequest()
+        self.config.include('pyramid_mailer.testing')
+        user.new_request_password_token(request)
+        self.failUnless(user.__token__())
+        
+    def test_remove_password_token(self):
+        user = self._cut()
+        user.__token__ = object()
+        user.remove_password_token()
+        self.assertIsNone(user.__token__)
 
 #FIXME:
-#test title = first + last name
 #All other accessors / mutators
+
+class RequestPasswordTokenTests(unittest.TestCase):
+    def setUp(self):
+        self.config = testing.setUp()
+
+    def tearDown(self):
+        testing.tearDown()
+    
+    def _make_obj(self):
+        from madetomeasure.models.users import RequestPasswordToken
+        return RequestPasswordToken()
+
+    def test_initial_values(self):
+        obj = self._make_obj()
+        self.assertEqual(obj.created + timedelta(days=3), obj.expires)
+
+    def test_call_returns_token(self):
+        obj = self._make_obj()
+        self.assertEqual(obj(), obj.token)
+        self.assertEqual(len(obj()), 30)
+    
+    def test_validate_works(self):
+        obj = self._make_obj()
+        obj.token = 'dummy'
+        obj.validate('dummy')
+    
+    def test_validate_expired(self):
+        obj = self._make_obj()
+        obj.token = 'dummy'
+        obj.expires = datetime.utcnow() - timedelta(days=1)
+        self.assertRaises(ValueError, obj.validate, 'dummy')
+
+    def test_validate_wrong_token(self):
+        obj = self._make_obj()
+        obj.token = 'dummy'
+        self.assertRaises(ValueError, obj.validate, 'wrong')
