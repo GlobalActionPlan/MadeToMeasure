34d05573379f95b665ad70569cf88e5b8c3dfac9
Fredrik Nilson <sassur@gmail.com>
2011-11-30 15:05:52 +0100

    test for users model

--- madetomeasure/models/tests/test_users.py
+++ madetomeasure/models/tests/test_users.py
@@ -21,11 +21,24 @@ class UsersTests(unittest.TestCase):
         from madetomeasure.models.users import Users
         return Users
 
+    def _add_users(self, users):
+        from madetomeasure.models.users import User
+        for u in ('kalle', 'olle', 'stina', 'lisa'):
+            user = User()
+            user.set_field_value('email', u+'@test.com')
+            users[u] = user
+
     def test_verify_class(self):
         self.failUnless(verifyClass(IUsers, self._cut))
 
     def test_verify_obj(self):
         self.failUnless(verifyObject(IUsers, self._cut()))
+        
+    def test_get_user_by_email(self):
+        users = self._cut()
+        self._add_users(users)
+        self.assertIsNotNone(users.get_user_by_email('olle@test.com'))
+        
 
 #FIXME:
 #All other accessors / mutators
