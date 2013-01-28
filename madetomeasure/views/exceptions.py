9334a8eff80552985dfabc87e6c529a11a726619
robinharms <robin@betahaus.net>
2013-01-28 15:56:57 +0100

    Templates for exceptions, and a redirect to login for 403 exc

---
+++ madetomeasure/views/exceptions.py
@@ -0,0 +1,25 @@
+from pyramid.view import view_config
+from pyramid.httpexceptions import HTTPForbidden
+from pyramid.httpexceptions import HTTPNotFound
+from pyramid.httpexceptions import HTTPFound
+
+from madetomeasure import MadeToMeasureTSF as _
+
+
+class ExceptionsView(object):
+
+    def __init__(self, exception, request):
+        self.exception = exception
+        self.request = request
+        self.response = {}
+
+    @view_config(context=HTTPForbidden)
+    def forbidden_view(self):
+        return HTTPFound(location = "/login")
+
+    @view_config(context=HTTPNotFound, renderer="templates/exceptions.pt")
+    def not_found_view(self):
+        #FIXME: Write a proper 404 page
+        self.response['msg'] = _(u"not_found_error_text",
+                                 u"Couldn't find that page.")
+        return self.response
