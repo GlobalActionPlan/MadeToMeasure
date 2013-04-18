9a221449d3838dcdbd057472d7acfeeec56257a0
robinharms <robin@betahaus.net>
2013-04-18 13:41:07 +0200

    Sections in manage questions now collapsible

--- madetomeasure/static/authenticated.js
+++ madetomeasure/static/authenticated.js
@@ -29,3 +29,28 @@ $(document).ready(function() {
         tag_select.find("option[value='']").attr('selected', 'selected'); //Don't trigger event
     })
 });
+/* Minimize
+ * Structure to make minimize work. elem can be most html tags
+ * <elem id="something_unique" class="toggle_area toggle_closed"> <!--or toggle_opened -->
+ *   <elem class="toggle_minimize">Something clickable</elem> <!-- if this allso has the class reload the page is reloaded after maximizing
+ *   <elem class="minimizable_area">Stuff that will be hidden</elem>
+ *   <elem class="minimizable_inverted">Stuff that will only be visible when it's minimized</elem>
+ * </elem>
+ */
+$('.toggle_minimize').live('click', function(event) {
+    try { event.preventDefault(); } catch(e) {}
+    min_parent = $(this).parents('.toggle_area');
+    // set cookie for opened or closed
+    var cookie_id = min_parent.attr('id');
+    if($(this).hasClass('reload')) {
+        if (min_parent.hasClass('toggle_opened')) {
+            $.cookie(cookie_id, 1, { expires: 7, path: '/'});
+        } else {
+            $.cookie(cookie_id, null, { expires: 7, path: '/'});
+            location.reload();
+        }
+    }
+    // Set parent class as opened or closed
+    min_parent.toggleClass('toggle_opened').toggleClass('toggle_closed');
+})
+
