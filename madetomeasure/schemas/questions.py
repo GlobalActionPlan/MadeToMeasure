52807ec60bd97bb0b3e2936dd2136ef003bce2ac
robinharms <robin@betahaus.net>
2013-12-10 15:42:52 +0100

    Wrong error text. Closes #40

--- madetomeasure/schemas/questions.py
+++ madetomeasure/schemas/questions.py
@@ -22,7 +22,7 @@ class TagsSequence(colander.SequenceSchema):
     text = colander.SchemaNode(
         colander.String(),
         preparer = adjust_tags,
-        validator = colander.All(colander.Length(max=100), colander.Regex(r'^\w*$', msg = _(u"Only letters and characters a-z allowed."))),
+        validator = colander.All(colander.Length(max=100), colander.Regex(r'^\w*$', msg = _(u"Only numbers, characters a-z and underscore allowed."))),
         widget=deferred_tags_text_widget,
         description=_(u"Enter some text"))
 
