84b4386631c72848426b74a6d0c344fcff193c65
robinharms <robin@betahaus.net>
2014-03-17 13:41:09 +0100

    Correct dependencies for fanstatic

--- madetomeasure/fanstaticlib.py
+++ madetomeasure/fanstaticlib.py
@@ -1,23 +1,18 @@
 from fanstatic import (Library,
                        Resource,
                        Group)
-from js.deform import (deform_basic,
-                       resource_mapping)
 from js.jqueryui import ui_sortable
 from js.deform_bootstrap import deform_bootstrap_js
-from js.bootstrap import bootstrap
+from js.bootstrap import (bootstrap,
+                          bootstrap_theme,)
 
-#Patch resource mapping to fetch deform basics instead of beautify.css
-#resource_mapping['deform'] = [deform_basic,]
 
 m2m_lib = Library('m2m_lib', 'static')
 
-#reset_css = Resource(m2m_lib, 'reset.css') reset_css
-main_css = Resource(m2m_lib, 'main.css', depends = (deform_basic, bootstrap,))
+main_css = Resource(m2m_lib, 'main.css', depends = (bootstrap_theme, bootstrap,))
 
-main_js = Resource(m2m_lib, 'main.js', depends = (bootstrap, deform_bootstrap_js,))
+main_js = Resource(m2m_lib, 'main.js', depends = (deform_bootstrap_js,))
 manage_questions = Resource(m2m_lib, 'manage_questions.js', depends = (main_js, ui_sortable,))
 
 survey_managers_resources = Group([main_css, main_js])
 survey_participants_resources = Group([main_css, main_js])
-#FIXME: Auto_need currently fetched beautify.css from deform. We might need to override that.
